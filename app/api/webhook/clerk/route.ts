2c9b1f6bb60c50d1c25586c2ed807a42235c8dfc
Sumit Mahat <mahatsumit5@gmail.com>
2023-12-17 18:10:53 +1100

    check

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -56,6 +56,7 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
+    console.log("user has been created");
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
