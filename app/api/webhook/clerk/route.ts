7c5552bb3d5f8590e0c438ebdbab07af89e7587d
Sumit Mahat <mahatsumit5@gmail.com>
2023-12-17 18:04:16 +1100

    databse bug

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,9 +54,11 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
+
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
+
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
@@ -65,7 +67,9 @@ export async function POST(req: Request) {
       lastName: last_name,
       photo: image_url,
     };
+
     const newUser = await createUser(user);
+
     if (newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
